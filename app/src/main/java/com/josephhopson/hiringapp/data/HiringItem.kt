05c25ddd2605aa2614c88dca568c98702f791837
Joseph Hopson <727468+philote@users.noreply.github.com>
2025-01-16 08:02:10 -0700

    Update HiringItem.kt
    code cleanup

--- app/src/main/java/com/josephhopson/hiringapp/data/HiringItem.kt
+++ app/src/main/java/com/josephhopson/hiringapp/data/HiringItem.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class HiringItem (
-    val id : Int = -1,
-    val listId : Int = -1,
-    val name : String? = null
+    val id: Int = -1,
+    val listId: Int = -1,
+    val name: String? = null
 )
